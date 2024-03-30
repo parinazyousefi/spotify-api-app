6faff680362d5c0bcd5378eb3549fc219ad6a6e3
ParinazYousefi <parinazyousefi1@gmail.com>
2024-03-30 16:25:13 -0400

    created,hardcoded,and styled playlist component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import Playlist from './components/Playlist/Playlist';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Playlist/>
     </div>
   );
 }
