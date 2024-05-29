cf7100a069226d9352c673a89e43d6a6c57339fc
ParinazYousefi <parinazyousefi1@gmail.com>
2024-05-29 19:55:04 -0400

    refactor for deployment

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,32 +1,16 @@
-import "./Login.scss";
-import { useEffect } from "react";
-import axios from "axios";
-
+import './Login.scss';
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-const Login = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        // Fetch the Spotify authentication URL from the backend
-        const response = await axios.get(`${backendUrl}/auth/spotify`);
-        // Redirect the user to the Spotify authentication page
-        window.location.href = response.data.authUrl;
-      } catch (error) {
-        console.error('Error fetching authentication URL:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  return (
-    <div className='login'>
-      <h1 className='login__title'>What's your mood ?</h1>
-      {/* Displaying a loading message or spinner could be helpful here */}
-      <p>Loading...</p>
-    </div>
-  );
+const Login=()=>{
+    const handleLogin=()=>{
+        window.location.href = `${backendUrl}/auth/spotify`;
+    }
+    return(
+        <div className='login'>
+            <h1 className='login__title'>What's your mood ?</h1>
+            <button className='login__btn' onClick={handleLogin}>Connect to Spotify</button>
+        </div>
+    )
 }
 
 export default Login;
