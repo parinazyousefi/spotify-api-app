f19fcf14b1b6bbed144ed644b44acde1ec045e4e
ParinazYousefi <parinazyousefi1@gmail.com>
2024-05-29 20:02:23 -0400

    change the login flow

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,16 +1,25 @@
-import './Login.scss';
+import "./Login.scss";
+import { useState } from "react";
+import axios from "axios";
+
 const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
-const Login=()=>{
-    const handleLogin=()=>{
-        window.location.href = `${backendUrl}/auth/spotify`;
+const Login = () => {
+  const handleLogin = async () => {
+    try {
+      // Redirect the user to the Spotify authentication page with the backend URL as a query parameter
+      window.location.href = `${backendUrl}/auth/spotify?redirect_uri=${encodeURIComponent(window.location.origin)}`;
+    } catch (error) {
+      console.error('Error initiating authentication:', error);
     }
-    return(
-        <div className='login'>
-            <h1 className='login__title'>What's your mood ?</h1>
-            <button className='login__btn' onClick={handleLogin}>Connect to Spotify</button>
-        </div>
-    )
+  };
+
+  return (
+    <div className='login'>
+      <h1 className='login__title'>What's your mood ?</h1>
+      <button className='login__btn' onClick={handleLogin}>Connect to Spotify</button>
+    </div>
+  );
 }
 
 export default Login;
